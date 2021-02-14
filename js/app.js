378154d6e5c2dbf5e00cfd37a3611ed6628d83ce
mikeovas <mikeovas@gmail.com>
2021-02-14 16:34:55 -0500

    started JS to create interactive search

--- js/app.js
+++ js/app.js
@@ -1,35 +1,52 @@
-window.addEventListener('load', function() {
-    baguetteBox.run('.gallery');
-  });
+window.addEventListener('load', function () {
+  baguetteBox.run('.gallery');
+});
 
 
 
 
-// loops through the data captions for each picture. Store as a variable and log to the console.
-  const prefix = 'photo';
-  for(i=1; i<=12; i++) {
+// Find user text input, store as variable and log to console.
+function searchInput() {
+  const userInput = document.getElementById('search');
+  const keyInput = userInput.value.toLowerCase();
+  console.log(keyInput);
+}
 
-  //Find user text input, store as variable and log to console.
-    function searchInput() {
-        const userInput = document.getElementById('search');
-        const keyInput = userInput.value.toLowerCase(); 
-        console.log(keyInput);
-    };
 
-    const dataCaptions = document.getElementById('photo' + i);
-    const captions = dataCaptions.getAttribute("data-caption").toLowerCase();
-    console.log(captions);
+// Loop thru data captions for each picture
+// Store as a variable and log to the console.
 
-    // let images = document.getElementsByTagName('img');
+const prefix = 'photo';
+for (i = 1; i <= 12; i++) {
+  const str = searchInput();
 
-  
-    //     console.log(images);
-    //     console.log(dataCaption);
-   
-      
+  const dataCaptions = document.getElementById('photo' + i);
+  const captions = dataCaptions.getAttribute("data-caption").toLowerCase();
+  console.log(captions);
+
+  // Loop thru data captions and filter out what not needed
+  const contained = captions.includes(str);
+  console.log(contained);
+
+  if (contained) {
+    console.log('input is found in the caption');
+  } else {
+    console.log('input is not found in the caption');
   }
-  
 
-// }
 
-// saveTextAsFile();
+};
+
+
+
+  // let images = document.getElementsByTagName('img');
+
+
+  //     console.log(images);
+  //     console.log(dataCaption);
+
+
+
+
+
+
