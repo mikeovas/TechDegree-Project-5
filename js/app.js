10d57b03912a627bb3fde5d9759a16949ebf4e60
mikeovas <mikeovas@gmail.com>
2021-02-14 13:35:34 -0500

    js code for checking input and looping through captions

--- js/app.js
+++ js/app.js
@@ -2,4 +2,31 @@ window.addEventListener('load', function() {
     baguetteBox.run('.gallery');
   });
 
-  const search = new Filter('search', 'data-caption');
+
+
+
+// loops through the data captions for each picture. Store as a variable and log to the console.
+  const prefix = 'photo';
+  for(i=1; i<=12; i++) {
+
+  //Find user text input, store as variable and log to console.
+    function searchInput() {
+        const userInput = document.getElementById('search');
+        const keyInput = userInput.value.toLowerCase(); 
+        console.log(keyInput);
+    };
+
+    const dataCaptions = document.getElementById('photo' + i);
+    const captions = dataCaptions.getAttribute("data-caption").toLowerCase();
+    console.log(captions);
+
+    
+
+   
+      
+  }
+  
+
+// }
+
+// saveTextAsFile();
