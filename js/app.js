80da9f4e2d57f0ef189472f64185181f07556bb9
mikeovas <mikeovas@gmail.com>
2021-02-14 18:24:11 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,47 +5,49 @@ window.addEventListener('load', function () {
 
 
 
+
 // Find user text input, store as variable and log to console.
 function searchInput() {
+
   const userInput = document.getElementById('search');
   const keyInput = userInput.value.toLowerCase();
   console.log(keyInput);
-  
-}
 
-const str = searchInput();
+ 
 
-// Loop thru data captions for each picture
-// Store as a variable and log to the console.
+  const prefix = 'photo';
+  for (i = 1; i <= 12; i++) {
 
-const prefix = 'photo';
-for (i = 1; i <= 12; i++) {
+    // Loop thru data captions for each picture and store as a variable and log to the console.    
+    const dataCaptions = document.getElementById('photo' + i);
+    const captions = dataCaptions.getAttribute("data-caption").toLowerCase();
+    console.log(captions);
 
-  const dataCaptions = document.getElementById('photo' + i);
-  const captions = dataCaptions.getAttribute("data-caption").toLowerCase();
-  console.log(captions);
+    // Loop thru data captions and filter out what not needed
+    const contained = captions.includes(keyInput);
+    console.log(contained);
+    
+    // display correct images
+    const images = document.getElementsByTagName('img');
 
-  // Loop thru data captions and filter out what not needed
-  const contained = captions.includes(str);
-  console.log(contained);
-  console.log(str);  // str is undefined...not getting an input in time for a comparison????
+    if (contained) {
+      images[i].style.innerHTML.display = "none"; 
+    } else {
+      images[i].style.innerHTML.display = "block";  
+    }
 
-  if (contained) {
-    console.log('input is found in the caption');
-  } else {
-    console.log('input is not found in the caption');
+    
   }
+
 
 
 };
 
 
 
-  // let images = document.getElementsByTagName('img');
+  
 
 
-  //     console.log(images);
-  //     console.log(dataCaption);
 
 
 
